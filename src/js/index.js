9787293fd1d0620fa075a12655f271beb300088a
Yuri Pedrozo <yurijogaizi@gmail.com>
2024-03-13 14:04:04 -0300

    Added Cart

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ document.querySelector("#ok-btn").addEventListener("click", function(){
     var texto = input.value;
 
     if(texto === "3273575b3") {
-        window.location.replace("https://consultadecarteirinhas.github.io/Carteirinha/user.html")
+        window.location.replace("https://consultadecarteirinhas.github.io/Carteirinha-Estudante/user_3273575b3.html")
+    }else if(texto === "3284675b2"){
+        window.location.replace("https://consultadecarteirinhas.github.io/Carteirinha-Estudante/user_3284675b2.html")
     }
 })
