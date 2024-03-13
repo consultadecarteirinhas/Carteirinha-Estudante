d862fd99a84b1368c9305f969dc53b84c7d662e5
Yuri Pedrozo <yurijogaizi@gmail.com>
2024-03-13 14:42:35 -0300

    index fixed

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,25 @@ document.querySelector("#ok-btn").addEventListener("click", function(){
         window.location.replace("https://consultadecarteirinhas.github.io/Carteirinha-Estudante/user_3284675b2.html")
     }
 })
+
+//Array de parametros 'chave=valor'
+var params = window.location.search.substring(1).split('&');
+
+//Criar objeto que vai conter os parametros
+var paramArray = {};
+
+//Passar por todos os parametros
+for(var i=0; i<params.length; i++) {
+    //Dividir os parametros chave e valor
+    var param = params[i].split('=');
+
+    //Adicionar ao objeto criado antes
+    paramArray[param[0]] = param[1];
+
+    var myInput = document.getElementById("carteirinha")
+    myInput.value = params[1]
+}
+
+
+console.log(myInput.value)
+
