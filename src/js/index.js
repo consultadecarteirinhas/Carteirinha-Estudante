53632a0d3428794c8afe16a859c7610895c5a331
Yuri Pedrozo <125318143+yuriown@users.noreply.github.com>
2024-08-27 21:15:07 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,8 @@ document.querySelector("#ok-btn").addEventListener("click", function(){
         window.location.replace("https://consultadecarteirinhas.github.io/Carteirinha-Estudante/user_3192675b3.html")
     }else if(texto === "2413101b3"){
         window.location.replace("https://consultadecarteirinhas.github.io/Carteirinha-Estudante/user_2413101b3.html")
+    }else if(texto === "2411303b3"){
+        window.location.replace("https://consultadecarteirinhas.github.io/Carteirinha-Estudante/user_2411303b3.html")
     }
 })
 
